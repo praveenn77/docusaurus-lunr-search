aaa8df4fe087f4c3bac4c20d840e3fd6a8bcc11a
David Marquis <davidmarquis@me.com>
2022-08-29 12:11:21 -0400

    Move test-specific input data to the test itself
    Also fixed a few typos and re-worded test description.

--- src/__tests__/utils.spec.js
+++ src/__tests__/utils.spec.js
@@ -2,18 +2,19 @@ const assert = require('assert')
 const utils = require('../utils')
 
 const baseUrl = 'http://example.com/'
-const routesPaths = [
-  `${baseUrl}docs/tutorial/overview`,
-  `${baseUrl}docs/tutorial/get-started`,
-  `${baseUrl}docs/how-to/add-plugin`,
-  `${baseUrl}docs/how-to/extract-value`,
-  `${baseUrl}docs/explanation/solar-sytem`,
-  `${baseUrl}docs/changelogs/index`,
-  `${baseUrl}docs/changelogs/rovers/lunar`,
-]
 
 describe('utils', () => {
-  it('should exlude routes by exludeRoutes globs', () => {
+  it('should not include routes matching globs provided in `excludeRoutes` options', () => {
+    const routesPaths = [
+      `${baseUrl}docs/tutorial/overview`,
+      `${baseUrl}docs/tutorial/get-started`,
+      `${baseUrl}docs/how-to/add-plugin`,
+      `${baseUrl}docs/how-to/extract-value`,
+      `${baseUrl}docs/explanation/solar-system`,
+      `${baseUrl}docs/changelogs/index`,
+      `${baseUrl}docs/changelogs/rovers/lunar`,
+    ]
+
     const [files, meta] = utils.getFilePaths(routesPaths, '/out', baseUrl, {
       excludeRoutes: ['docs/changelogs/**/*']
     })
@@ -23,7 +24,7 @@ describe('utils', () => {
       `docs/tutorial/get-started`,
       `docs/how-to/add-plugin`,
       `docs/how-to/extract-value`,
-      `docs/explanation/solar-sytem`,
+      `docs/explanation/solar-system`,
     ])
 
     assert.equal(meta.excludedCount, 2)
